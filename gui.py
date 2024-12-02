3358df5b536d9382021f0921049e8c3e8fce3311
Mallory Sorola <127247712+MalloryAnn@users.noreply.github.com>
2024-12-01 19:56:01 -0600

    Update gui.py

--- gui.py
+++ gui.py
@@ -9,7 +9,7 @@ def connect_to_database():
         conn = mysql.connector.connect(
             host="localhost",
             user="root",
-            password="Princess55$$!!",
+            password="your pw here",
             database="HotelManagementSystem"
         )
         return conn
